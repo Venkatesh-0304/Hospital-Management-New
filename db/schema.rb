38c9e9a1751a70fcee1b5e502f7f78bbd4cb3f83
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-07-06 17:59:41 +0530

    Commit One on trial

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_26_042759) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_26_070205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "appointments", force: :cascade do |t|
+    t.date "appointment_date"
     t.datetime "created_at", null: false
     t.bigint "doctor_id", null: false
     t.bigint "patient_id", null: false
