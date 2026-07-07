1bafc174d19f5ebca94d3515359161c9d20ee6b5
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-07-07 09:54:40 +0530

    doctor-route modification

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :doctors
+  resources :doctors, only[:index, :show, :edit]
   resources :patients
   resources :hospitals
   resources :appointments
