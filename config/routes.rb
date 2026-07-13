51f2dd2247ec9823dac5a6ec4252b85c496059ae
LalithaMangalamM <lalli155003@gmail.com>
2026-07-13 10:43:17 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :doctors, only[:index, :show, :edit]
+  resources :doctors
   resources :patients
   resources :hospitals
   resources :appointments
