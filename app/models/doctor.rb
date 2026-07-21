a969894948684cc46707060496926f825a0fc7df
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-07-21 15:49:20 +0530

    Patient association changed

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,6 +3,6 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_one :profile, dependent: :destroy
   has_many :appointments, dependent: :destroy
-  has_many :patients, through: :appointment
+  has_many :patients, through: :appointments
   accepts_nested_attributes_for :profile
 end
